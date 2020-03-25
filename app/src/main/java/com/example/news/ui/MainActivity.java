82091d17366deb518d9ec2e7e88e80e67c80d9b4
surajverma0619 <suraj@fiblabs.com>
2020-03-25 19:28:15 +0530

    app: network setup check

--- app/src/main/java/com/example/news/ui/MainActivity.java
+++ app/src/main/java/com/example/news/ui/MainActivity.java
@@ -6,14 +6,24 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.news.R;
+import com.example.news.model.Hits;
 import com.example.news.model.NewsData;
 import com.example.news.repo.NewsRepository;
 
 public class MainActivity extends AppCompatActivity {
     private NewsRepository newsRepository = new NewsRepository();
     private LiveData<NewsData> ldNewsData;
+    private RecyclerView newsRecyclerView;
+    private NewsAdapter newsAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private Hits[] hits;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +32,33 @@ public class MainActivity extends AppCompatActivity {
         newsRepository = new NewsRepository();
 
         ldNewsData = newsRepository.getNewsDataOnline("story", 1);
-        setObserver();
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swlNews);
 
+        newsRecyclerView = (RecyclerView) findViewById(R.id.rvNews);
+        layoutManager = new LinearLayoutManager(this);
+        newsAdapter = new NewsAdapter(this,hits);
+        newsRecyclerView.setLayoutManager(layoutManager);
+        newsRecyclerView.setAdapter(newsAdapter);
+        setObserver();
+        refreshListener();
+    }
+
+    private void refreshListener(){
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                setObserver();
+
+            }
+        });
     }
 
     private void setObserver() {
         ldNewsData.observe(this,newsData -> {
-            newsData.getExhaustiveNbHits();
+            hits = newsData.getHits();
+            newsAdapter.setData(hits);
+            swipeRefreshLayout.setRefreshing(false);
+            ldNewsData.removeObservers(this);
         });
     }
 }
--- app/src/main/java/com/example/news/ui/NewsAdapter.java
+++ app/src/main/java/com/example/news/ui/NewsAdapter.java
@@ -1,5 +1,78 @@
 package com.example.news.ui;
 
-public class NewsAdapter {
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.news.R;
+import com.example.news.model.Hits;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+    private Context context;
+    private Hits[] hits;
+    public int mSelectedNews = 0;
+
+    public NewsAdapter(Context context, Hits[] hits) {
+        this.context = context;
+        this.hits = hits;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_news_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String title = hits[position].getTitle();
+        String date = hits[position].getCreatedAt();
+        holder.tvTitle.setText(title);
+        holder.tvDate.setText(date);
+
+        holder.cvNewsListItem.setOnClickListener(view -> {
+            holder.switchNews.toggle();
+
+        });
+
+    }
+
+    public void setData(Hits[] hits) {
+        this.hits = hits;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (hits== null){
+            return 0;
+        }else{
+            return hits.length;
+        }
+
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvTitle = itemView.findViewById(R.id.tvTitle);
+        TextView tvDate = itemView.findViewById(R.id.tvDate);
+        CardView cvNewsListItem = itemView.findViewById(R.id.cvNewsListItem);
+        Switch switchNews = itemView.findViewById(R.id.switchListItem);
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 
 }
